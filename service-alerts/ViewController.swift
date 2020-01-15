c1347fd35cbd8c979311bb680226a569ff7d39bc
Rances Lacayo <crlacayo@protonmail.ch>
2020-01-14 18:44:29 -0600

    Initial Commit

---
+++ service-alerts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  service-alerts
+//
+//  Created by Rances Lacayo on 1/14/20.
+//  Copyright © 2020 Rances Lacayo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
